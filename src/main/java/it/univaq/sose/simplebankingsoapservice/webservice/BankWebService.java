baedcebdaef8cedb120ea57ba1d05b5e8b960dd6
Stefano Decina <stefano.decina1@gmail.com>
2024-04-24 14:39:29 +0200

    getAllAccountsAndBankAccounts, depositMoneyInBankAccount and withdrawMoneyInBankAccount methods

--- src/main/java/it/univaq/sose/simplebankingsoapservice/webservice/BankWebService.java
+++ src/main/java/it/univaq/sose/simplebankingsoapservice/webservice/BankWebService.java
@@ -1,10 +1,12 @@
 package it.univaq.sose.simplebankingsoapservice.webservice;
 
 import it.univaq.sose.simplebankingsoapservice.dto.AccountAndBankAccount;
+import it.univaq.sose.simplebankingsoapservice.dto.MoneyTransfer;
 import it.univaq.sose.simplebankingsoapservice.dto.OpenBankAccountRequest;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import java.util.List;
 
 @WebService
 public interface BankWebService {
@@ -13,4 +15,18 @@ public interface BankWebService {
 
     @WebMethod
     public AccountAndBankAccount saveAccountAndBankAccount(OpenBankAccountRequest request);
+
+    @WebMethod
+    public List<AccountAndBankAccount> getAllAccountsAndBankAccounts(); // TODO Filtro solo per customer (Da verificare)
+
+    @WebMethod
+    public AccountAndBankAccount depositMoneyInBankAccount(MoneyTransfer moneyTransfer);
+
+    @WebMethod
+    public AccountAndBankAccount withdrawMoneyInBankAccount(MoneyTransfer moneyTransfer);
+
+    //TODO delete account-bankAccount (add stato conto) -> cancellazione (add last index method)
+
+    //TODO Creazione account di servizio senza bank account
+
 }
--- src/main/java/it/univaq/sose/simplebankingsoapservice/webservice/BankWebServiceImpl.java
+++ src/main/java/it/univaq/sose/simplebankingsoapservice/webservice/BankWebServiceImpl.java
@@ -2,10 +2,16 @@ package it.univaq.sose.simplebankingsoapservice.webservice;
 
 import it.univaq.sose.simplebankingsoapservice.domain.Account;
 import it.univaq.sose.simplebankingsoapservice.domain.BankAccount;
+import it.univaq.sose.simplebankingsoapservice.domain.Role;
 import it.univaq.sose.simplebankingsoapservice.dto.AccountAndBankAccount;
+import it.univaq.sose.simplebankingsoapservice.dto.MoneyTransfer;
 import it.univaq.sose.simplebankingsoapservice.dto.OpenBankAccountRequest;
 import it.univaq.sose.simplebankingsoapservice.repository.AccountRepository;
 import it.univaq.sose.simplebankingsoapservice.repository.BankAccountRepository;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 public class BankWebServiceImpl implements BankWebService {
     private AccountRepository accountRepository = AccountRepository.getInstance();
@@ -15,10 +21,10 @@ public class BankWebServiceImpl implements BankWebService {
     public AccountAndBankAccount getAccountAndBankAccount(long accountId) {
         Account account = accountRepository.findById(accountId);
         BankAccount bankAccount = bankAccountRepository.findById(account.getIdBankAccount());
-        System.out.println("Risposta");
-        AccountAndBankAccount andBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), bankAccount);
-        System.out.println(andBankAccount);
-        return andBankAccount;
+        System.out.println("Risposta getAccountAndBankAccount");
+        AccountAndBankAccount accountAndBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), account.getUsername(), bankAccount);
+        System.out.println(accountAndBankAccount);
+        return accountAndBankAccount;
     }
 
     @Override
@@ -27,15 +33,67 @@ public class BankWebServiceImpl implements BankWebService {
         long idAccount = accountRepository.lastIdSave() + 1;
         long idBankAccount = bankAccountRepository.lastIdSave() + 1;
         String bankAccountNumber = bankAccountRepository.generateNewBankAccountNumber();
-        Account account = new Account(idAccount, request.getName(), request.getSurname());
+        Account account = new Account(idAccount, request.getName(), request.getSurname(), Role.CUSTOMER, request.getUsername(), request.getPassword());
         idAccount = accountRepository.save(account);
         BankAccount bankAccount = new BankAccount(idBankAccount, bankAccountNumber, request.getMoney(), idAccount);
         bankAccountRepository.save(bankAccount);
         account.setIdBankAccount(bankAccount);
         accountRepository.save(account);
-        System.out.println("Risposta");
-        AccountAndBankAccount andBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), bankAccount);
+        System.out.println("Risposta saveAccountAndBankAccount");
+        AccountAndBankAccount andBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), account.getUsername(), bankAccount);
         System.out.println(andBankAccount);
         return andBankAccount;
+    }
+
+    @Override
+    public List<AccountAndBankAccount> getAllAccountsAndBankAccounts() {
+        List<AccountAndBankAccount> accountsAndBankAccounts = new LinkedList<>();
+
+        List<Account> accounts = accountRepository.findAll();
+        List<BankAccount> bankAccounts = bankAccountRepository.findAll();
+
+        /*accounts.forEach(account -> accountsAndBankAccounts.add(
+                new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), bankAccounts.stream().filter(
+                        bankAccount -> bankAccount.getIdBankAccount() == account.getIdBankAccount()).findFirst().orElse(null))));*/
+
+        accounts.forEach(account -> {
+            Optional<BankAccount> matchingBankAccount = bankAccounts.stream()
+                    .filter(bankAccount -> bankAccount.getIdBankAccount() == account.getIdBankAccount())
+                    .findFirst();
+
+            matchingBankAccount.ifPresent(bankAccount -> accountsAndBankAccounts.add(new AccountAndBankAccount(
+                    account.getIdAccount(),
+                    account.getName(),
+                    account.getSurname(),
+                    account.getUsername(),
+                    bankAccount
+            )));
+        });
+
+        System.out.println("Risposta getAllAccountsAndBankAccounts");
+        System.out.println(accountsAndBankAccounts);
+        return accountsAndBankAccounts;
+    }
+
+    @Override
+    public AccountAndBankAccount depositMoneyInBankAccount(MoneyTransfer moneyTransfer) {
+        bankAccountRepository.addMoney(moneyTransfer.getIdBankAccount(), moneyTransfer.getAmount());
+        BankAccount bankAccount = bankAccountRepository.findById(moneyTransfer.getIdBankAccount());
+        Account account = accountRepository.findById(bankAccount.getAccount());
+        AccountAndBankAccount accountAndBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), account.getUsername(), bankAccount);
+        System.out.println("Risposta depositMoneyInBankAccount");
+        System.out.println(accountAndBankAccount);
+        return accountAndBankAccount;
+    }
+
+    @Override
+    public AccountAndBankAccount withdrawMoneyInBankAccount(MoneyTransfer moneyTransfer) {
+        bankAccountRepository.removeMoney(moneyTransfer.getIdBankAccount(), moneyTransfer.getAmount());
+        BankAccount bankAccount = bankAccountRepository.findById(moneyTransfer.getIdBankAccount());
+        Account account = accountRepository.findById(bankAccount.getAccount());
+        AccountAndBankAccount accountAndBankAccount = new AccountAndBankAccount(account.getIdAccount(), account.getName(), account.getSurname(), account.getUsername(), bankAccount);
+        System.out.println("Risposta withdrawMoneyInBankAccount");
+        System.out.println(accountAndBankAccount);
+        return accountAndBankAccount;
     }
 }
